d1154f1c73f274394d1a5c9b1ccf9405f80ae8c7
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-20 18:59:25 +0530

    Update __init__.py

--- bot/helpers/sql_helper/__init__.py
+++ bot/helpers/sql_helper/__init__.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from lib.sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from bot import DATABASE_URL, LOGGER
 
