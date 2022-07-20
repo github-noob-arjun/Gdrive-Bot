f8c76304efcf016f56cc289fdc39329ce56dcd19
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-20 16:27:53 +0530

    Update Gbot.py

--- bot/Gbot.py
+++ bot/Gbot.py
@@ -1,12 +1,10 @@
 import os
 import logging
 from pyrogram import Client
-from bot import (
-  APP_ID,
-  API_HASH,
-  BOT_TOKEN,
-  DOWNLOAD_DIRECTORY
-  )
+
+BOT_TOKEN = ""
+APP_ID = "
+API_HASH = ""
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -16,11 +14,8 @@ LOGGER = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 GdriveBot = Client(
-    "G-DriveBot",
-    bot_token=BOT_TOKEN,
-    api_id=APP_ID,
-    api_hash=API_HASH
-    # plugins=plugins,
-    # parse_mode="markdown",
-    # workdir=DOWNLOAD_DIRECTORY
+      "G-DriveBot",
+      bot_token="5318793234:AAHtik1DzBw7qpL_YZcWI_i3M9E1Noxm9w0",
+      api_id="6015447",
+      api_hash="0e96dd0dd4c4c9ded27c2ef58e6ab112"
 )
