c680ecc13f3fa91c70f1aa73f006bd145c975536
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-20 11:18:46 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bot',
+      version='0.2.1',
+      description='Python module for interacting with Chatbase APIs',
+      url='https://github.com/github-noob-arjun/Gdrive-Bot',
+      author='Github',
+      author_email='None',
+      license='MIT Pro',
+      packages=['bot'],      
+      zip_safe=False)
