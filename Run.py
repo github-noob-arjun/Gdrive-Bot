a60d2dd30b9d8a65c4e24045c77eb4250af90c2b
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2024-07-27 15:06:42 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,8 +1,41 @@
-from pyrogram import Client
-from bot.config import *
 import os
-from aiohttp import web
-from bot.web import web_server
+import logging
+from pyrogram import Client
+from bot import (
+  APP_ID,
+  API_HASH,
+  BOT_TOKEN,
+  DOWNLOAD_DIRECTORY
+  )
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+LOGGER = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+if __name__ == "__main__":
+    if not os.path.isdir(DOWNLOAD_DIRECTORY):
+        os.makedirs(DOWNLOAD_DIRECTORY)
+    plugins = dict(
+        root="bot/plugins"
+    )
+    GdriveBot = Client(
+        "G-DriveBot",
+        bot_token=BOT_TOKEN,
+        api_id=APP_ID,
+        api_hash=API_HASH,
+        plugins=plugins,
+        parse_mode="markdown",
+        workdir=DOWNLOAD_DIRECTORY
+    )
+    LOGGER.info('Starting Bot !')
+    GdriveBot.run()
+    LOGGER.info('Bot Stopped !')
+
+#--------
 
 WEB_SUPPORT = "True"
 
